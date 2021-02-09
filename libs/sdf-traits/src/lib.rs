bda814d264e61d7643c15bcf998bdde7d2329279
Lando <lando.schumpich@gmail.com>
2021-02-09 19:44:59 +0100

    Updates sdf-traits with circle primitive

--- libs/sdf-traits/src/lib.rs
+++ libs/sdf-traits/src/lib.rs
@@ -1,109 +1,19 @@
+use sdf_vecs::{Vec3, Vec1, VecType, ops::{Length, add_high}, ComponentAccess};
 use std::ops::DerefMut;
 
-struct Vec2 {
-    x: f32,
-    y: f32
-}
-
-impl Vec2 {
-    fn cross_product(&self, other: &Vec2) -> Vec2 {
-        todo!()
-    }
-}
-
-#[derive(Clone, Copy)]
-struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32
-}
-
-impl Vec3 {
-    fn cross_product(&self, other: &Vec3) -> Vec3 {
-        todo!()
-    }
-
-    fn length(&self) -> f32 {
-        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
-    }
-}
-
-impl Default for Vec3 {
-    fn default() -> Self {
-        Self {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0
-        }
-    }
-}
-
-struct Vec4 {
-    x: f32,
-    y: f32,
-    z: f32,
-    w: f32
-}
-
-impl Vec4 {
-    fn cross_product(&self, other: &Vec4) -> Vec4 {
-        todo!()
-    }
-}
-
-/// All possible vec dimensions
-enum Vec {
-    Vec2(Vec2),
-    Vec3(Vec3),
-    Vec4(Vec4)
-}
-
-impl Vec {
-    fn cross_product(a: &Vec, b: &Vec) -> Vec {
-        match a {
-            Vec::Vec2(va) => match b {
-                Vec::Vec2(vb) => Vec::Vec2(va.cross_product(vb)),
-                _ => unreachable!()
-            },
-            Vec::Vec3(va) => match b {
-                Vec::Vec3(vb) => Vec::Vec3(va.cross_product(vb)),
-                _ => unreachable!()
-            },
-            Vec::Vec4(va) => match b {
-                Vec::Vec4(vb) => Vec::Vec4(va.cross_product(vb)),
-                _ => unreachable!()
-            },
-        }
-    }
-
-    fn length(&self) -> f32 {
-        todo!()
-    }
-}
-
-/// A variable in a function
-enum Variable {
-    NumConst(Box<dyn Algebraic>),
-    VecVar
-}
-
 enum VecVar {
-    VecConst(Box<dyn Spatial>),
-    Replaceable(Vec3)
+    Operator(Box<dyn Spatial>),
+    Replaceable(Vec3),
+    Const(VecType)
 }
 
-/// After performing an operation, there can't be a replaceable variable left,
-/// As it will have been consumed
-enum EvaluatedVariable {
-    VecConst(Vec),
-    NumConst(f32)
-}
-
-impl std::ops::Add<EvaluatedVariable> for EvaluatedVariable {
-    type Output = EvaluatedVariable;
-
-    fn add(self, rhs: EvaluatedVariable) -> Self::Output {
-        todo!()
+impl VariableContainer for VecVar {
+    fn replace_variable(&mut self, var: &Vec3) {
+        match self {
+            VecVar::Operator(o) => o.replace_variable(var),
+            VecVar::Replaceable(r) => *r = *var,
+            VecVar::Const(_) => ()
+        }
     }
 }
 
@@ -116,148 +26,144 @@ trait Operator<T> {
     fn operate(&self) -> T;
 }
 
-/// Operators that always return numbers
-trait Algebraic: Operator<f32> + VariableContainer { }
+/// Operators that always return a VecType
+trait Spatial: Operator<VecType> + VariableContainer { }
 
-/// Operators that always return a Vec
-trait Spatial: Operator<Vec> + VariableContainer { }
+struct NoOp(VecVar);
 
-/// Operators that have differing return types depending on their input
-trait Mixed: Operator<EvaluatedVariable> + VariableContainer { }
-
-struct Add {
-    lhs: Variable,
-    rhs: Variable
-}
-
-impl VariableContainer for Add {
+impl VariableContainer for NoOp {
     fn replace_variable(&mut self, var: &Vec3) {
-        todo!()
+        self.0.replace_variable(var)
     }
 }
 
-impl Mixed for Add { }
-
-impl Operator<EvaluatedVariable> for Add {
-    /// phew thats **a lot** of matching, but it makes sense, we want to support all those cases
-    fn operate(&self) -> EvaluatedVariable {
-        // match &self.lhs {
-        //     Variable::NumConst(n_l) => match &self.rhs {
-        //         Variable::NumConst(n_r) => EvaluatedVariable::NumConst(n_l.operate() + n_r.operate()),
-        //         Variable::VecConst(v_r) => EvaluatedVariable::VecConst(n_l.operate() + v_r.operate()),
-        //         Variable::Replaceable(r_r) => EvaluatedVariable::VecConst(n_l.operate() + r_r)
-        //     },
-        //     Variable::VecConst(v_l) => match &self.rhs {
-        //         Variable::NumConst(n_r) => EvaluatedVariable::VecConst(v_l.operate() + n_r.operate()),
-        //         Variable::VecConst(v_r) => EvaluatedVariable::VecConst(v_l.operate() + v_r.operate()),
-        //         Variable::Replaceable(r_r) => EvaluatedVariable::VecConst(v_l.operate() + r_r)
-        //     },
-        //     Variable::Replaceable(r_l) => match &self.rhs {
-        //         Variable::NumConst(n_r) => EvaluatedVariable::VecConst(r_l + n_r.operate()),
-        //         Variable::VecConst(v_r) => EvaluatedVariable::VecConst(r_l + v_r.operate()),
-        //         Variable::Replaceable(r_r) => EvaluatedVariable::VecConst(r_l + r_r)
-        //     }
-        // }
-        todo!()
-    }
-}
-
-struct Atan2 {
-    lhs: Box<dyn Algebraic>,
-    rhs: Box<dyn Algebraic>
-}
-
-impl VariableContainer for Atan2 {
-    /// Actually makes no sense here, as atan2 never takes a vec arg
-    fn replace_variable(&mut self, var: &Vec3) {
-        todo!()
-    }
-}
-
-impl Algebraic for Atan2 {}
-
-impl Operator<f32> for Atan2 {
-    fn operate(&self) -> f32 {
-        self.lhs.operate().atan2(self.rhs.operate())
-    }
-}
-
-struct CrossProduct {
-    lhs: VecVar,
-    rhs: VecVar,
-}
-
-impl VariableContainer for CrossProduct {
-    fn replace_variable(&mut self, var: &Vec3) {
-        todo!()
-    }
-}
-
-impl Spatial for CrossProduct {}
-
-impl Operator<Vec> for CrossProduct {
-    fn operate(&self) -> Vec {
-        todo!()
-    }
-}
-
-struct NumConst(f32);
-
-impl Algebraic for NumConst { }
-
-impl VariableContainer for NumConst {
-    fn replace_variable(&mut self, var: &Vec3) {
-        todo!()
-    }
-}
-
-impl Operator<f32> for NumConst {
-    fn operate(&self) -> f32 {
-        self.0
-    }
-}
-
-struct Length(VecVar);
-
-impl Algebraic for Length { }
-
-impl VariableContainer for Length {
-    fn replace_variable(&mut self, var: &Vec3) {
-        match &mut self.0 {
-            VecVar::Replaceable(v) => *v = *var,
-            VecVar::VecConst(v) => v.replace_variable(var)
-        }
-    }
-}
-
-impl Operator<f32> for Length {
-    fn operate(&self) -> f32 {
+impl Operator<VecType> for NoOp {
+    fn operate(&self) -> VecType {
         match &self.0 {
-            VecVar::VecConst(v) => v.operate().length(),
-            VecVar::Replaceable(v) => v.length()
+            VecVar::Replaceable(r) => VecType::Vec3(*r),
+            VecVar::Operator(v) => v.operate(),
+            VecVar::Const(c) => *c
         }
     }
 }
 
-struct SDF {
-    root: Box<dyn Algebraic>
+struct AddOp {
+    lhs: VecVar,
+    rhs: VecVar
 }
 
-impl SDF {
+impl Spatial for AddOp {}
+
+impl VariableContainer for AddOp {
+    fn replace_variable(&mut self, var: &Vec3) {
+        self.lhs.replace_variable(var);
+        self.rhs.replace_variable(var);
+    }
+}
+
+impl Operator<VecType> for AddOp {
+    fn operate(&self) -> VecType {
+        match &self.lhs {
+            VecVar::Replaceable(v_l) => match &self.rhs {
+                VecVar::Replaceable(v_r) => (*v_l + *v_r).into(),
+                VecVar::Operator(v_r) => add_high(&VecType::Vec3(*v_l), &v_r.operate()),
+                VecVar::Const(c) => add_high(&VecType::Vec3(*v_l), &c)
+            },
+            VecVar::Operator(o_l) => match &self.rhs {
+                VecVar::Operator(o_r) => add_high(&o_r.operate(), &o_l.operate()),
+                VecVar::Replaceable(v_r) => add_high(&o_l.operate(), &VecType::Vec3(*v_r)),
+                VecVar::Const(c) => add_high(&o_l.operate(), &c)
+            },
+            VecVar::Const(c_l) => match &self.rhs {
+                VecVar::Const(c_r) => add_high(&c_l, &c_r),
+                VecVar::Operator(o) => add_high(&c_l, &o.operate()),
+                VecVar::Replaceable(r) => add_high(&c_l, &VecType::Vec3(*r))
+            }
+        }
+    }
+}
+
+struct LengthOp(VecVar);
+
+impl Spatial for LengthOp { }
+
+impl VariableContainer for LengthOp {
+    fn replace_variable(&mut self, var: &Vec3) {
+        self.0.replace_variable(var);
+    }
+}
+
+impl Operator<VecType> for LengthOp {
+    fn operate(&self) -> VecType {
+        match &self.0 {
+            VecVar::Replaceable(r) => Vec1::new(r.length()).into(),
+            VecVar::Operator(o) => Vec1::new(o.operate().length()).into(),
+            VecVar::Const(c) => Vec1::new(c.length()).into()
+        }
+    }
+}
+
+struct NegOp(VecVar);
+
+impl Spatial for NegOp { }
+
+impl VariableContainer for NegOp {
+    fn replace_variable(&mut self, var: &Vec3) {
+        self.0.replace_variable(var)
+    }
+}
+
+impl Operator<VecType> for NegOp {
+    fn operate(&self) -> VecType {
+        match &self.0 {
+            VecVar::Operator(v) => -v.operate(),
+            VecVar::Replaceable(v) => VecType::Vec3(-*v),
+            VecVar::Const(c) => -*c
+        }
+    }
+}
+
+struct TraitSDF {
+    root: Box<dyn Spatial>
+}
+
+impl TraitSDF {
     pub fn sign_at(&mut self, position: &Vec3) -> f32 {
         // insert position for variable
         self.root.deref_mut().replace_variable(position);
 
         // operate the whole tree and return
-        self.root.operate()
+        match self.root.operate() {
+            VecType::Vec1(v) => v.x(),
+            _ => unreachable!()
+        }
 
     }
 
     fn var_length() -> Self {
-        let length = Length(VecVar::Replaceable(Vec3::default()));
+        let length = LengthOp(VecVar::Replaceable(Vec3::default()));
 
         Self {
             root: Box::new(length)
+        }
+    }
+
+    fn circle(center: &Vec3, radius: f32) -> Self {
+        // length(P-C)-r, where P is query point, C is Center vec and r is radius
+        let center_neg = NegOp(VecVar::Const(VecType::Vec3(*center)));
+        let center_var_sub = AddOp {
+            lhs: VecVar::Replaceable(Vec3::default()),
+            rhs: VecVar::Operator(Box::new(center_neg))
+        };
+        let dist_from_center = LengthOp(VecVar::Operator(Box::new(center_var_sub)));
+        let radius_neg = NegOp(VecVar::Const(Vec1::new(radius).into()));
+        let radius_sub = AddOp {
+            lhs: VecVar::Operator(Box::new(dist_from_center)),
+            rhs: VecVar::Operator(Box::new(radius_neg))
+        };
+
+        Self {
+            root: Box::new(radius_sub)
         }
     }
 }
@@ -268,10 +174,19 @@ mod tests {
 
     #[test]
     fn length_works() {
-        let mut sdf = SDF::var_length();
+        let mut sdf = TraitSDF::var_length();
 
-        assert_eq!(1.0, sdf.sign_at(&Vec3{x: 1.0, y: 0.0, z: 0.0}));
-        assert_eq!(1.0, sdf.sign_at(&Vec3{x: -1.0, y: 0.0, z: 0.0}));
-        assert_eq!(2.0f32.sqrt(), sdf.sign_at(&Vec3{x: -1.0, y: 1.0, z: 0.0}));
+        assert_eq!(1.0, sdf.sign_at(&Vec3::new(1.0, 0.0, 0.0)));
+        assert_eq!(1.0, sdf.sign_at(&Vec3::new(-1.0, 0.0, 0.0)));
+        assert_eq!(2.0f32.sqrt(), sdf.sign_at(&Vec3::new(-1.0, 1.0, 0.0)));
+    }
+
+    #[test]
+    fn circle_works() {
+        let mut sdf = TraitSDF::circle(&Vec3::new(0.0, -1.0, 0.0), 10.0);
+
+        assert_eq!(-10.0, sdf.sign_at(&Vec3::new(0.0, -1.0, 0.0)));
+        assert_eq!(0.0, sdf.sign_at(&Vec3::new(10.0, -1.0, 0.0)));
+        assert_eq!(10.0, sdf.sign_at(&Vec3::new(20.0, -1.0, 0.0)));
     }
 }
