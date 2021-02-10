e1ef194254a1be6d7413c3181398ff5d076cf629
Lando <lando.schumpich@gmail.com>
2021-02-10 12:54:06 +0100

    Add component acces ops for sdf-traits

--- libs/sdf-traits/src/ops/mod.rs
+++ libs/sdf-traits/src/ops/mod.rs
@@ -3,9 +3,11 @@ mod add;
 mod neg;
 mod length;
 mod sub;
+mod swizzle;
 
 pub(crate) use self::noop::NoOp;
 pub(crate) use self::add::Add;
 pub(crate) use self::neg::Neg;
 pub(crate) use self::length::Length;
 pub(crate) use self::sub::Sub;
+pub(crate) use self::swizzle::{X, Y, Z, W};
